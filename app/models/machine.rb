43e944e82c9429f82225e3623e31c46c91748012
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-04 12:00:46 -0600

    add average snacks feature

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,4 +1,8 @@
 class Machine < ApplicationRecord
   belongs_to :owner
   has_and_belongs_to_many :snacks
+
+  def average_snacks
+    snacks.average(:price)
+  end
 end
--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -20,4 +20,14 @@ feature 'When a user visits a vending machine show page' do
     expect(page).to have_content("White Castle Burger: $3.5")
     expect(page).to have_content("Pop Rocks: $1.5")
   end
+  scenario 'they see the average price of all snacks associated with that machine' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    dons.snacks.create!(snack: "White Castle Burger", price: "3.50")
+    dons.snacks.create!(snack: "Pop Rocks", price: "1.50")
+
+    visit machine_path(dons)
+    #note: price formatting is a little weird right now
+    expect(page).to have_content("Average Snack Price: $2.5")
+  end
 end
