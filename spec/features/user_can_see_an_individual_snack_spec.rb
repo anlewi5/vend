62a91478edbc18583369ea585ac1101d25191b4e
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-04 12:16:55 -0600

    add snacks route and controller with show action

--- spec/features/user_can_see_an_individual_snack_spec.rb
+++ spec/features/user_can_see_an_individual_snack_spec.rb
@@ -1,26 +1,39 @@
 require 'rails_helper'
 
 feature 'When a user visits a snack show page' do
-  scenario 'they see the snack name' do
+  before do
     owner = Owner.create(name: "Sam's Snacks")
+
     dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    matts  = owner.machines.create(location: "Matt's Un-mixed Drinks")
 
-    visit machine_path(dons)
+    dons.snacks.create!(snack: "White Castle Burger", price: "3.50")
+    dons.snacks.create!(snack: "Pop Rocks", price: "1.50")
+    dons.snacks.create!(snack: "Pop Rocks", price: "1.50")
 
-    expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+    snack = matts.snacks.create!(snack: "White Castle Burger", price: "3.50")
+
+    visit snack_path(snack)
+  end
+
+  scenario 'they see the snack name' do
+    expect(page).to have_content("White Castle Burger")
   end
   scenario 'they see the snack price' do
-
+    expect(page).to have_content("$3.5")
   end
   scenario "they see information about vending machines with that snack" do
-    scenario 'they see the snack locations of vending machines with the snack' do
-
+    scenario 'they see the locations of vending machines with the snack' do
+      expect(page).to have_content("Don's Mixed Drinks")
+      expect(page).to have_content("Matt's Un-mixed Drinks")
     end
     scenario 'they see the average price for snacks in those vending machines' do
-
+      expect(page).to have_content("Average Snack Price: $2.5")
+      expect(page).to have_content("Average Snack Price: $3.5")
     end
     scenario 'they see a count of the different kinds of items in that vending machine' do
-
+      expect(page).to have_content("Available: 1")
+      expect(page).to have_content("Available: 1")
     end
   end
 end
